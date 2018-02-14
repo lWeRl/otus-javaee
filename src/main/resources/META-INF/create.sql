fbf867db4a4ac262aa951401e0ce1654e44cbfb7
lWeRl <werasdas@gmail.com>
2018-02-14 19:34:51 +0300

    HomeWork2 MultiLine SQL

--- src/main/resources/META-INF/create.sql
+++ src/main/resources/META-INF/create.sql
@@ -1,5 +1,35 @@
-CREATE TABLE IF NOT EXISTS "department" (id BIGSERIAL PRIMARY KEY NOT NULL,name VARCHAR(20) NOT NULL,city VARCHAR(20) NOT NULL);
-CREATE TABLE IF NOT EXISTS "position" (id BIGSERIAL PRIMARY KEY NOT NULL,name VARCHAR(20) NOT NULL,salary FLOAT NOT NULL);
-CREATE TABLE IF NOT EXISTS "employee" (id BIGSERIAL PRIMARY KEY NOT NULL,lastName VARCHAR(20) NOT NULL,firstName VARCHAR(20) NOT NULL,middleName VARCHAR(20) NOT NULL,login VARCHAR(20) NOT NULL,password VARCHAR(20) NOT NULL,department_id BIGINT REFERENCES department(id),position_id BIGINT REFERENCES position(id));
+CREATE TABLE IF NOT EXISTS "department" (
+  id   BIGSERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(20)           NOT NULL,
+  city VARCHAR(20)           NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "position" (
+  id     BIGSERIAL PRIMARY KEY NOT NULL,
+  name   VARCHAR(20)           NOT NULL,
+  salary FLOAT                 NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "employee" (
+  id            BIGSERIAL PRIMARY KEY NOT NULL,
+  lastName      VARCHAR(20)           NOT NULL,
+  firstName     VARCHAR(20)           NOT NULL,
+  middleName    VARCHAR(20)           NOT NULL,
+  login         VARCHAR(20)           NOT NULL,
+  password      VARCHAR(20)           NOT NULL,
+  department_id BIGINT REFERENCES department (id),
+  position_id   BIGINT REFERENCES position (id)
+);
 
-CREATE OR REPLACE FUNCTION getMaxSalaryName() RETURNS VARCHAR AS $$ DECLARE result varchar; BEGIN SELECT lastName INTO result FROM employee JOIN "position" ON employee.position_id = position.id WHERE "position".salary = (SELECT max(salary) FROM "position"); RETURN result; END $$ LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION getMaxSalaryName()
+  RETURNS VARCHAR AS
+$$
+DECLARE result VARCHAR;
+BEGIN
+  SELECT lastName
+  INTO result
+  FROM employee
+    JOIN "position" ON employee.position_id = position.id
+  WHERE "position".salary = (SELECT max(salary)
+                             FROM "position");
+  RETURN result;
+END
+$$ LANGUAGE plpgsql;
--- src/main/resources/META-INF/data.sql
+++ src/main/resources/META-INF/data.sql
@@ -11,17 +11,28 @@ INSERT INTO "position" (id, name, salary) VALUES (4, 'Manager', 250);
 INSERT INTO "position" (id, name, salary) VALUES (5, 'Fat Manager', 300);
 SELECT setval('position_id_seq', 5);
 -- Employers
-
-INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id) VALUES (1, 'lastname1', 'firstname1', 'middlename1', 'login1', 'password1', 1, 1);
-INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id) VALUES (2, 'lastname2', 'firstname2', 'middlename2', 'login2', 'password2', 2, 2);
-INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id) VALUES (3, 'lastname3', 'firstname3', 'middlename3', 'login3', 'password3', 3, 2);
-INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id) VALUES (4, 'lastname4', 'firstname4', 'middlename4', 'login4', 'password4', 1, 3);
-INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id) VALUES (5, 'lastname5', 'firstname5', 'middlename5', 'login5', 'password5', 2, 3);
-INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id) VALUES (6, 'lastname6', 'firstname6', 'middlename6', 'login6', 'password6', 3, 4);
-INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id) VALUES (7, 'lastname7', 'firstname7', 'middlename7', 'login7', 'password7', 1, 4);
-INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id) VALUES (8, 'lastname8', 'firstname8', 'middlename8', 'login8', 'password8', 2, 4);
-INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id) VALUES (9, 'lastname9', 'firstname9', 'middlename9', 'login9', 'password9', 3, 4);
-INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id) VALUES (10, 'lastname10', 'firstname10', 'middlename10', 'login10', 'password10', 1, 5);
-INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id) VALUES (11, 'lastname11', 'firstname11', 'middlename11', 'login11', 'password11', 2, 5);
-INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id) VALUES (12, 'lastname12', 'firstname12', 'middlename12 ', 'login12', 'password12', 3, 5);
+INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id)
+VALUES (1, 'lastname1', 'firstname1', 'middlename1', 'login1', 'password1', 1, 1);
+INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id)
+VALUES (2, 'lastname2', 'firstname2', 'middlename2', 'login2', 'password2', 2, 2);
+INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id)
+VALUES (3, 'lastname3', 'firstname3', 'middlename3', 'login3', 'password3', 3, 2);
+INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id)
+VALUES (4, 'lastname4', 'firstname4', 'middlename4', 'login4', 'password4', 1, 3);
+INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id)
+VALUES (5, 'lastname5', 'firstname5', 'middlename5', 'login5', 'password5', 2, 3);
+INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id)
+VALUES (6, 'lastname6', 'firstname6', 'middlename6', 'login6', 'password6', 3, 4);
+INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id)
+VALUES (7, 'lastname7', 'firstname7', 'middlename7', 'login7', 'password7', 1, 4);
+INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id)
+VALUES (8, 'lastname8', 'firstname8', 'middlename8', 'login8', 'password8', 2, 4);
+INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id)
+VALUES (9, 'lastname9', 'firstname9', 'middlename9', 'login9', 'password9', 3, 4);
+INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id)
+VALUES (10, 'lastname10', 'firstname10', 'middlename10', 'login10', 'password10', 1, 5);
+INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id)
+VALUES (11, 'lastname11', 'firstname11', 'middlename11', 'login11', 'password11', 2, 5);
+INSERT INTO "employee" (id, lastname, firstname, middlename, login, password, department_id, position_id)
+VALUES (12, 'lastname12', 'firstname12', 'middlename12 ', 'login12', 'password12', 3, 5);
 SELECT setval('employee_id_seq', 15);
